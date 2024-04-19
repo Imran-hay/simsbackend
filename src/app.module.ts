e07a41204e09b3673b5b5da95a309975ea6e8cc6
bereketsintayehu <bereketsintayehu32@gmail.com>
2024-04-19 10:37:40 +0300

    mongo url fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log(`${process.env.MONGOUSER}`)
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@cluster0.gpom6d9.mongodb.net/${process.env.MONGODB}?retryWrites=true&w=majority`,
+      `${process.env.MONGODBURI}`,
     ),
   ],
   controllers: [AppController],
